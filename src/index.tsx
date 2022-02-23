789071f3aa6aceb169dfbb16f9d34871e600b215
saurabhshankariya <sshankariya@gmail.com>
2022-02-23 10:48:21 +0530

    updates

--- src/index.tsx
+++ src/index.tsx
@@ -10,44 +10,49 @@ import {
 
 // This function detects most providers injected at window.ethereum
 import detectEthereumProvider from "@metamask/detect-provider";
+import { isPublicFacing } from "./utils/helpers";
 
 // const theme = createTheme(GLOBAL_APP_THEME);
 
 //Provider promise
 const provider = detectEthereumProvider();
 
-//check metamask plugin installed or not
-provider
-  .then((res: any) => {
-    if (
-      res &&
-      res.isMetaMask !== undefined &&
-      res.networkVersion !== undefined
-    ) {
-      // From now on, this should always be true:
-      // provider === window.ethereum
-      console.log(
-        "%c%s",
-        "color: white; background: green; font-size: 24px;font-weight:bold",
-        `${METAMASK_INSTALLED}`
-      );
-      ReactDOM.render(<App />, document.getElementById("root")); // initialize your app
-    } else {
+if (isPublicFacing()) {
+  ReactDOM.render(<App />, document.getElementById("root")); // initialize your app
+} else {
+  //check metamask plugin installed or not
+  provider
+    .then((res: any) => {
+      if (
+        res &&
+        res.isMetaMask !== undefined &&
+        res.networkVersion !== undefined
+      ) {
+        // From now on, this should always be true:
+        // provider === window.ethereum
+        console.log(
+          "%c%s",
+          "color: white; background: green; font-size: 24px;font-weight:bold",
+          `${METAMASK_INSTALLED}`
+        );
+        ReactDOM.render(<App />, document.getElementById("root")); // initialize your app
+      } else {
+        console.log(
+          "%c%s",
+          "color: white; background: red; font-size: 24px;font-weight:bold",
+          `${METAMASK_NOT_INSTALLED}`
+        );
+        ReactDOM.render(
+          <MetamaskComponent />,
+          document.getElementById("metamaskError")
+        ); // Notify client to install meta mask plugin
+      }
+    })
+    .catch((e: any) => {
       console.log(
         "%c%s",
         "color: white; background: red; font-size: 24px;font-weight:bold",
-        `${METAMASK_NOT_INSTALLED}`
+        "Metamask plugin detector is not working !"
       );
-      ReactDOM.render(
-        <MetamaskComponent />,
-        document.getElementById("metamaskError")
-      ); // Notify client to install meta mask plugin
-    }
-  })
-  .catch((e: any) => {
-    console.log(
-      "%c%s",
-      "color: white; background: red; font-size: 24px;font-weight:bold",
-      "Metamask plugin detector is not working !"
-    );
-  });
+    });
+}
